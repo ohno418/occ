af213f2295c6d26327660b97fd559e867dfbb007
taro0 <yutaro.ono.418@gmail.com>
2021-09-02 23:10:19 +0900

    Improve test code

--- test.sh
+++ test.sh
@@ -1,30 +1,22 @@
-# Case 1
-./occ 42 > tmp.s
-gcc -o tmp tmp.s
+assert() {
+  input=$1
+  expected=$2
 
-./tmp
+  ./occ "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual=$?
 
-ret="$?"
-if [ "$ret" = "42" ]; then
-  echo "42 => 42"
-else
-  echo "42 => $ret"
-  exit 1
-fi
+  if [ "$expected" = "$actual" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
 
-# Case 2
-./occ 123 > tmp.s
-gcc -o tmp tmp.s
-
-./tmp
-
-ret="$?"
-if [ "$ret" = "123" ]; then
-  echo "123 => 123"
-else
-  echo "123 => $ret"
-  exit 1
-fi
+assert "42" "42"
+assert "123" "123"
 
 echo OK
 exit 0
